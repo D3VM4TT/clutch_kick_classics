f430d346ef28f8b82e2de98b415f47216e7ca837
EulerSolutionsMatt <47004936+EulerSolutionsMatt@users.noreply.github.com>
2021-10-18 08:20:09 +0200

    Init repo & add command to create user

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User\AdminUser;
+use Doctrine\ORM\EntityManager;
+use Sylius\Bundle\CoreBundle\Fixture\Factory\AdminUserExampleFactory;
+use Sylius\Bundle\UserBundle\Factory\UserWithEncoderFactory;
+use Sylius\Component\Core\Model\AdminUserInterface;
+use Sylius\Component\User\Model\UserInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateAdminCommand extends Command
+{
+
+    protected static $defaultDescription = 'Creates a new Admin user.';
+
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @var UserWithEncoderFactory
+     */
+    private $userFactory;
+
+    /**
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em, UserWithEncoderFactory $userWithEncoderFactory)
+    {
+        $this->userFactory = $userWithEncoderFactory;
+        $this->em = $em;
+
+        parent::__construct();
+    }
+
+
+    protected function configure(): void
+    {
+        $this
+            // the command help shown when running the command with the "--help" option
+            ->setHelp('This command allows you to create a user...')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        /** @var AdminUserInterface $admin */
+        $admin = $this->userFactory->createNew();
+        $admin->setEmail('admin@admin.com');
+        $admin->setPlainPassword('admin');
+        $admin->setLocaleCode("en_US");
+        $admin->setEnabled(true);
+        $this->em->getRepository(AdminUser::class)->add($admin);
+        $output->writeln([
+            'Creating Admin User',
+            '============',
+            '',
+        ]);
+
+        return Command::SUCCESS;
+    }
+}
