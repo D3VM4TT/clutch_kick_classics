90d56315cbc425bc7e376cb5e89bf07045f29196
EulerSolutionsMatt <47004936+EulerSolutionsMatt@users.noreply.github.com>
2021-12-09 09:15:40 +0200

    Create new Sylius Payment Gateway

---
+++ src/Payum/SyliusApi.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Payum;
+
+final class SyliusApi
+{
+    /** @var string */
+    private $apiKey;
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function getApiKey(): string
+    {
+        return $this->apiKey;
+    }
+}
