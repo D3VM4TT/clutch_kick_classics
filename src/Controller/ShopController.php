6c683c24da9b10be4f370291e26ef30734859185
Matthew De Jager <matt.dj@soapmedia.co.uk>
2022-01-29 08:19:07 +0200

    Route refactor

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -14,61 +14,59 @@ use Symfony\Component\HttpFoundation\Response;
 class ShopController extends AbstractController
 {
 
-    public function termsAndConditionsAction(Request $request): Response
-    {
-        return $this->render('@SyliusShop/TermsAndConditions/index.html.twig', );
-    }
-
-    public function privacyPolicyAction(Request $request): Response
-    {
-        return $this->render('@SyliusShop/PrivacyPolicy/index.html.twig');
-    }
-
-    public function theCarAction(Request $request): Response
-    {
-        return $this->render('@SyliusShop/TheCar/show.html.twig');
-    }
-
     public function processPaymentAction($order_id, Request $request): Response
     {
-        // get the current order
-        $order = $this->get('doctrine.orm.default_entity_manager')->getRepository(Order::class)->find($order_id);
-        $stateMachineFactory = $this->container->get('sm.factory');
-        $orderCheckoutStateMachine = $stateMachineFactory->get($order, OrderCheckoutTransitions::GRAPH);
+        if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+            $origin = $_SERVER['HTTP_ORIGIN'];
+        }
+        else if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+            $origin = $_SERVER['HTTP_REFERER'];
+        } else {
+            $origin = $_SERVER['REMOTE_ADDR'];
+        }
 
-        $request->getSession()->set('sylius_order_id', $order->getId());
+        if (str_contains($origin, 'payfast.co.za')) {
+            // get the current order
+            $order = $this->get('doctrine.orm.default_entity_manager')->getRepository(Order::class)->find($order_id);
+            $stateMachineFactory = $this->container->get('sm.factory');
+            $orderCheckoutStateMachine = $stateMachineFactory->get($order, OrderCheckoutTransitions::GRAPH);
 
-        if ($order->getPaymentState() === 'paid') {
+            $request->getSession()->set('sylius_order_id', $order->getId());
+
+            if ($order->getPaymentState() === 'paid') {
+                return $this->redirectToRoute('sylius_shop_order_thank_you');
+            }
+
+            // ORDER CHECKOUT PROCESS
+
+            // --- 1. Select Shipping Method ---
+            $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SKIP_SHIPPING);
+
+            // --- 2. Select Payment ---
+            // Let's assume that you have a method with code 'paypal' configured
+            $paymentMethod = $this->container->get('sylius.repository.payment_method')->findOneByCode('payfast_payment');
+
+            // Payments are a Collection, so even though you have one Payment by default you have to iterate over them
+            foreach ($order->getPayments() as $payment) {
+                $payment->setMethod($paymentMethod);
+            }
+
+            $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SELECT_PAYMENT);
+            $this->container->get('sylius.manager.order')->flush();
+
+            // --- 3. Complete the Order ---
+            $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_COMPLETE);
+            $this->container->get('sylius.manager.order')->flush();
+
+            // --- 4. Pay for the order ---
+            $orderPaymentStateMachine = $stateMachineFactory->get($order, OrderPaymentTransitions::GRAPH);
+            $orderPaymentStateMachine->apply(OrderPaymentTransitions::TRANSITION_PAY);
+            $this->container->get('sylius.manager.order')->flush();
+
             return $this->redirectToRoute('sylius_shop_order_thank_you');
         }
 
-        // ORDER CHECKOUT PROCESS
-
-        // --- 1. Select Shipping Method ---
-        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SKIP_SHIPPING);
-
-        // --- 2. Select Payment ---
-        // Let's assume that you have a method with code 'paypal' configured
-        $paymentMethod = $this->container->get('sylius.repository.payment_method')->findOneByCode('payfast_payment');
-
-        // Payments are a Collection, so even though you have one Payment by default you have to iterate over them
-        foreach ($order->getPayments() as $payment) {
-            $payment->setMethod($paymentMethod);
-        }
-
-        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SELECT_PAYMENT);
-        $this->container->get('sylius.manager.order')->flush();
-
-        // --- 3. Complete the Order ---
-        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_COMPLETE);
-        $this->container->get('sylius.manager.order')->flush();
-
-        // --- 4. Pay for the order ---
-        $orderPaymentStateMachine = $stateMachineFactory->get($order, OrderPaymentTransitions::GRAPH);
-        $orderPaymentStateMachine->apply(OrderPaymentTransitions::TRANSITION_PAY);
-        $this->container->get('sylius.manager.order')->flush();
-
-        return $this->redirectToRoute('sylius_shop_order_thank_you');
+        return  $this->redirectToRoute('sylius_shop_homepage');
 
     }
 
